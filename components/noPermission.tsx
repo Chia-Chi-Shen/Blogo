ef2f079ccf40269a5ac865d1b2e6ddda820d3f09
Chia-Chi-Shen <r12227108@ntu.edu.tw>
2024-03-30 16:01:18 +0800

    fix no permission page

--- components/noPermission.tsx
+++ components/noPermission.tsx
@@ -3,7 +3,7 @@ export default function NoPermission() {
         <div className="h-screen w-screen flex justify-center items-center">
             <h1 className="text-4xl">No Permission</h1>
             <p className="text-lg">
-                You don't have permission to access this page. <br/>
+                You do not have permission to access this page. <br/>
                 Please change your permission on Github settings, and login again.
             </p>
         </div>
